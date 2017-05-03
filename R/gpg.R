03c18f0691efc40fb3436df4078bb3dead9af752
Alex Axthelm <AAxthelm@che.in.gov>
2017-05-03 13:50:51 -0400

    Add readr functions
    This commit closes #4, and I feel comfortable saying that I've done
    enought to close #1 as well.

--- R/gpg.R
+++ R/gpg.R
@@ -3,7 +3,15 @@ gpg_rds_encrypt <- function(x, recipient, path = "data"){
   x_name <- deparse(substitute(x))
   file_temp <- tempfile(fileext = ".RDS")
   file_encrypt <- file.path(path, paste0(x_name, ".RDS.gpg"))
-  saveRDS(object = x, file = file_temp)
+
+  if (requireNamespace("readr", quietly = TRUE)) {
+    # Maybe we can save some time here
+    readr::write_rds(x = x, path = file_temp)
+  } else {
+    # But also leave a failsafe option
+    saveRDS(object = x, file = file_temp)
+  }
+
   # Now we will call gpg to encrypt the file
   system(paste("gpg --recipient", recipient, "--output", file_encrypt, "--encrypt", file_temp))
   file.remove(file_temp)
@@ -12,7 +20,15 @@ gpg_rds_encrypt <- function(x, recipient, path = "data"){
 gpg_rds_decrypt <- function(encrypted_file){
   file_temp <- tempfile(fileext = ".RDS")
   system(paste("gpg --output", file_temp, "--decrypt", encrypted_file))
-  tmp <- readRDS(file_temp)
+
+  if (requireNamespace("readr", quietly = TRUE)) {
+    # Maybe we can save some time here
+    clear <- readr::read_rds(path = file_temp)
+  } else {
+    # But also leave a failsafe option
+    clear <- readRDS(file = file_temp)
+  }
+
   file.remove(file_temp)
-  return(tmp)
+  return(clear)
 }
