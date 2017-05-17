73c81fdf1493017ca872c523204268fcd01d85c3
Alex Axthelm <AAxthelm@che.in.gov>
2017-05-17 14:28:47 -0400

    Change to `hasArg` to try to determine if function has defined argument

--- R/time_check.R
+++ R/time_check.R
@@ -14,27 +14,25 @@ check_elapsed_time <- function(last_time, start_time){
   time_now <- proc.time()
 
   #Check if last_time exists
-  if (missing(last_time)){
-    last_time <- NULL
-  }
-  if (!exists(deparse(substitute(last_time)))){
-    last_time <- NULL
-  }
-  if (!is.null(last_time)){
-  elapsed_time <- round(time_now["elapsed"] - last_time["elapsed"], 2)
-  message(elapsed_time, " Seconds since last check")
+  if (hasArg(last_time)){
+    if (!exists(deparse(substitute(last_time)))){
+      last_time <- NULL
+    }
+    if (!is.null(last_time)){
+    elapsed_time <- round(time_now["elapsed"] - last_time["elapsed"], 2)
+    message(elapsed_time, " Seconds since last check")
+    }
   }
 
-  #Check if start_time exists
-  if (missing(start_time)){
-    start_time <- NULL
-  }
-  if (!exists(deparse(substitute(start_time)))){
-    last_time <- NULL
-  }
-  if (!is.null(start_time)){
+  #Check if last_time exists
+  if (hasArg(start_time)){
+    if (!exists(deparse(substitute(start_time)))){
+      start_time <- NULL
+    }
+    if (!is.null(start_time)){
     total_elapsed_time <- round(time_now["elapsed"] - start_time["elapsed"], 2)
     message(total_elapsed_time, " Seconds total")
+    }
   }
   return(proc.time())
 }
