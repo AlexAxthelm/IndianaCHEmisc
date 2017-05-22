feab51513f6a13d87baec270eeadf517f7faf8b2
Alex Axthelm <AAxthelm@che.in.gov>
2017-05-22 13:42:03 -0400

    Changes to satisfy `lintr`

--- R/term.R
+++ R/term.R
@@ -10,7 +10,7 @@ validate_termkey <- function(termkey){
   good_length <- nchar(termkey) == 5
   is_int_year <- !is.na(as.integer(key_year))
   is_int_term <- !is.na(as.integer(key_term))
-  is_good_summer_new <- !(key_term %in% c(1,4) & key_year >= 2017)
+  is_good_summer_new <- !(key_term %in% c(1, 4) & key_year >= 2017)
   is_good_summer_old <- !(key_term == 5 & key_year <= 2015)
   is_good_summer_2016 <- termkey != 20164
   key_term_valid <- key_term %in% 1:5
@@ -54,7 +54,8 @@ split_termkey <- function(termkey){
     is.na(termcode) ~ NA_character_
   )
 
-  # I know it seems like I'm reinventing the wheel here, to get back to the same term codes as we started with, but:
+  # I know it seems like I'm reinventing the wheel here, to get back to the
+  # same term codes as we started with, but:
   # 1: I want a sanity check
   # 2: I want to be sure that the (old) summer terms are smooshing correctly
   term_season_number <- dplyr::case_when(
@@ -65,7 +66,8 @@ split_termkey <- function(termkey){
   )
 
   fiscal_year <- ifelse(termcode == 1 & key_year == 2016, NA_integer_, key_year)
-  # In 2016 CHE stopped using the Summer AB distinction across fiscal years. The 20161 term is the last of the AB distinctions
+  # In 2016 CHE stopped using the Summer AB distinction
+  # across fiscal years. The 20161 term is the last of the AB distinctions
   fiscal_year <- ifelse(is.na(termkey), NA_integer_, fiscal_year)
 
   return_list <- list(
@@ -81,7 +83,8 @@ split_termkey <- function(termkey){
 }
 
 count_seasons <- function(termkey1, termkey2, neg = TRUE){
-  # neg = TRUE indicates that the function  will return a negative number if termkey1 is later than termkey2 (negative time)
+  # neg = TRUE indicates that the function  will return a negative number if
+  # termkey1 is later than termkey2 (negative time)
   # neg = FALSE will simply give the absolute difference
   #order the termkeys
   neg_factor <- 1
@@ -89,7 +92,9 @@ count_seasons <- function(termkey1, termkey2, neg = TRUE){
   termlist2 <- split_termkey(termkey2)
 
   if (termkey1 > termkey2) {
-    if (neg) {neg_factor <- -1}
+    if (neg) {
+      neg_factor <- -1
+    }
     foo <- termlist1
     termlist1 <- termlist2
     termlist2 <- foo
@@ -102,11 +107,11 @@ count_seasons <- function(termkey1, termkey2, neg = TRUE){
     termlist2$term_season == "Summer" & termlist1$term_season == "Fall" ~ +2,
     termlist2$term_season == "Summer" & termlist1$term_season == "Spring" ~ +1,
     termlist2$term_season == "Fall" & termlist1$term_season == "Spring" ~ -1,
-    termlist2$term_season == "Fall" & termlist1$term_season == "Summer" ~ -2, # Here we are rolling over to the next academic year
-    termlist2$term_season == "Spring" & termlist1$term_season == "Summer" ~ -1#,
-    # TRUE ~ NA_integer_ # This should never happen. I think I captured everything else
-    )
+    # Here we are rolling over to the next academic year
+    termlist2$term_season == "Fall" & termlist1$term_season == "Summer" ~ -2,
+    termlist2$term_season == "Spring" & termlist1$term_season == "Summer" ~ -1
+  )
 
-  num_seasons = (diff_years*3) + mod_seasons
+  num_seasons <- (diff_years * 3) + mod_seasons
   return(num_seasons * neg_factor)
 }
